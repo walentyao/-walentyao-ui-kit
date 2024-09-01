96a8a3cc93aed7fad52725a8dc314e21b77095d7
walentyao <prikolnyyyvalk@yandex.ru>
2024-09-02 02:52:34 +0300

    feat: изменен стиль кнопки outline

--- src/providers/WalentyaoUIProvider/WalentyaoUIProvider.tsx
+++ src/providers/WalentyaoUIProvider/WalentyaoUIProvider.tsx
@@ -1,10 +1,8 @@
-import classNames from 'classnames';
-import cls from './WalentyaoUIProvider.module.css';
 import '../../styles/index.css';
+
 interface WalentyaoProviderProps {
-  className?: string;
   children?: React.ReactNode;
 }
-export const WalentyaoUIProvider = ({ children, className }: WalentyaoProviderProps) => {
-  return <div className={classNames(cls.variables_colors, cls.provider, className)}>{children}</div>;
+export const WalentyaoUIProvider = ({ children }: WalentyaoProviderProps) => {
+  return <>{children}</>;
 };
