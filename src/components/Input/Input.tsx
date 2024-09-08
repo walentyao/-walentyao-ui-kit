de2cb9990c4290f284a0ae4ff5bcf52f3cb915e8
walentyao <prikolnyyyvalk@yandex.ru>
2024-09-08 19:53:44 +0300

    feat: добавлено состояние valid и пропсы для управления компонентом

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,37 +1,70 @@
 import classNames from 'classnames';
 import { InputSizeType } from '../../types/input.types';
-import cls from './Input.module.css';
 import { useId, useMemo } from 'react';
+import cls from './Input.module.css';
 
 interface InputProps {
   id?: string;
+  value?: string;
+  size?: InputSizeType;
+  valid?: boolean;
+  icon?: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
   label?: string;
   description?: string;
-  size?: InputSizeType;
   placeholder?: string;
+  className?: string;
+  onChange?: (value: string) => void;
 }
 
 const inputSizeStyles = (size: InputSizeType) => classNames(cls[`inputSize${size.toUpperCase()}`]);
 
-export const Input = ({ id, label, description, placeholder, size = 's' }: InputProps) => {
+export const Input = ({
+  id,
+  value,
+  label,
+  description,
+  placeholder,
+  className,
+  icon: Icon,
+  size = 's',
+  valid = true,
+  onChange,
+}: InputProps) => {
   const defaultId = useId();
   const inputId = useMemo(() => id || defaultId, [defaultId, id]);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onChange?.(e.target.value);
+  };
   return (
-    <div className={classNames(cls.input, inputSizeStyles(size))}>
+    <div className={classNames(cls.input, inputSizeStyles(size), className)}>
       {label && (
         <label
           htmlFor={inputId}
-          className={cls.inputLabel}
+          className={classNames(cls.inputLabel, {
+            [cls.inputLabelInvalid]: !valid,
+          })}
         >
           {label}
         </label>
       )}
-      <input
-        id={inputId}
-        type="text"
-        placeholder={placeholder}
-        className={classNames(cls.inputField)}
-      />
+      <div className={cls.inputFieldWrapper}>
+        {Icon && (
+          <label htmlFor={inputId}>
+            <Icon className={cls.inputFieldIcon}></Icon>
+          </label>
+        )}
+        <input
+          id={inputId}
+          value={value}
+          onChange={handleChange}
+          type="text"
+          placeholder={placeholder}
+          className={classNames(cls.inputField, {
+            [cls.inputInvalid]: !valid,
+            [cls.inputFieldWithIcon]: !!Icon,
+          })}
+        />
+      </div>
       {description && <p className={cls.inputDescription}>{description}</p>}
     </div>
   );
